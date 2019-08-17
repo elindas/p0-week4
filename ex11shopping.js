765ed867a6203a25c0a2b368084ddf6679443a40
Elinda Sintaresmi <elinda.sinta@gmail.com>
2019-08-17 08:34:18 +0700

    add

---
+++ ex11shopping.js
@@ -0,0 +1,63 @@
+
+function shoppingTime(memberId, money) {
+    var sisa = money;
+    var belanja = [];
+
+    for (var i = 0; i < 5; i++) {
+        if (sisa >= 1500000) {
+            belanja.push('Sepatu Stacattu');
+            sisa -= 1500000;
+        } else if (sisa >= 500000) {
+            belanja.push('Baju Zoro');
+            sisa -= 500000;
+        } else if (sisa >= 250000) {
+            belanja.push('Baju H&N');
+            sisa -= 250000;
+        } else if (sisa >= 175000) {
+            belanja.push('Sweater Uniklooh');
+            sisa -= 175000;
+        } else if (sisa >= 50000) {
+            belanja.push('CasingHandphone');
+            sisa -= 50000;
+            break;
+        }
+    }
+
+
+    var objProduct = {};
+    objProduct.memberId = memberId;
+    objProduct.money = money;
+    objProduct.listPurchased = belanja;
+    objProduct.changeMoney = sisa;
+
+
+    if (memberId === '' || memberId === undefined && money === undefined) {
+        return 'Mohon maaf, toko X hanya berlaku untuk member saja';
+    } else if (money < 50000) {
+        return 'Mohon maaf, uang tidak cukup';
+    }
+
+
+    return objProduct;
+}
+
+// TEST CASES
+console.log(shoppingTime('1820RzKrnWn08', 2475000));
+//{ memberId: '1820RzKrnWn08',
+// money: 2475000,
+// listPurchased:
+//  [ 'Sepatu Stacattu',
+//    'Baju Zoro',
+//    'Baju H&N',
+//    'Sweater Uniklooh',
+//    'Casing Handphone' ],
+// changeMoney: 0 }
+console.log(shoppingTime('82Ku8Ma742', 170000));
+//{ memberId: '82Ku8Ma742',
+// money: 170000,
+// listPurchased:
+//  [ 'Casing Handphone' ],
+// changeMoney: 120000 }
+console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
+console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
+console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
