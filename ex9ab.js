799b6377767f54d687c511046789b1ecfac01883
Elinda Sintaresmi <elinda.sinta@gmail.com>
2019-08-21 00:48:09 +0700

    add

--- ex9ab.js
+++ ex9ab.js
@@ -1,25 +1,20 @@
-
-
-
-function checkAB(str) {
-  var a=0
-  var b=0
-  for (var i = 0; i < str.length; i++) {
-    if (str[i] === 'a') {
-      a = i
-    } else if (str[i] === 'b') {
-      b = i
+function checkAB(num) {
+  for (var i = 0; i < num.length; i++) {
+    if (num[i] === "a" && num[i + 4] === "b") {
+      return true;
+    } else if (num[i] === "b" && num[i + 4] === "a") {
+      return true;
     }
   }
-  return b - a === 4 || a - b === 4
+  return false;
 
   // atau dengan cara
   // return /a[\S\s]{3}b/.test(str)
 }
 
 // TEST CASES
-console.log(checkAB('lane borrowed')); // true
-console.log(checkAB('i am sick')); // false
-console.log(checkAB('you are boring')); // true
-console.log(checkAB('barbarian')); // true
-console.log(checkAB('bacon and meat')); // false
+console.log(checkAB("lane borrowed")); // true
+console.log(checkAB("i am sick")); // false
+console.log(checkAB("you are boring")); // true
+console.log(checkAB("barbarian")); // true
+console.log(checkAB("bacon and meat")); // false
